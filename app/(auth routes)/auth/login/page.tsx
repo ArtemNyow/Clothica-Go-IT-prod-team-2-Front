d4cdf3652d68764232f49b6415f8fa5390c270e0
Oleksandr Honchar <figules@gmail.com>
2025-11-08 22:48:03 +0100

    refactor(auth): simplify LoginPage component structure

--- app/(auth routes)/auth/login/page.tsx
+++ app/(auth routes)/auth/login/page.tsx
@@ -1,9 +1,5 @@
 import LoginForm from "@/components/auth/LoginForm";
 
 export default function LoginPage() {
-  return (
-    <section className="min-h-screen flex items-center justify-center">
-      <LoginForm />
-    </section>
-  );
+  return (<LoginForm />);
 }
