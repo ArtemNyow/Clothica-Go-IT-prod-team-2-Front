58d443f98076051ac759883a9e9461a47e9b7e02
Yasmin <jana.afonina@gmail.com>
2025-11-11 21:04:39 +0200

    next

--- components/StyleFeatures/StyleFeatures.tsx
+++ components/StyleFeatures/StyleFeatures.tsx
@@ -1,32 +1,31 @@
-
 import styles from './StyleFeatures.module.css';
  
 export default function Style() {
   return (
-    <section className={css.style}>
-      <div className={css.container}>
-        <h2 className={css.title}>Обери свій унікальний стиль сьогодні</h2>
-        <ul className={css.list}>
-          <li className={css.item}>
-            <div className={`${css.icon} ${css.iconNatural}`}></div>
-            <h3 className={css.cardTitle}>Якість та натуральність</h3>
-            <p className={css.description}>
+    <section className={styles.style}>
+      <div className={styles.container}>
+        <h2 className={styles.title}>Обери свій унікальний стиль сьогодні</h2>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <div className={`${styles.icon} ${styles.iconNatural}`}></div>
+            <h3 className={styles.cardTitle}>Якість та натуральність</h3>
+            <p className={styles.description}>
               тільки приємні до тіла тканини, які зберігають форму навіть після
               десятків прань.
             </p>
           </li>
-          <li className={css.item}>
-            <div className={`${css.icon} ${css.iconUniversal}`}></div>
-            <h3 className={css.cardTitle}>Універсальний дизайн</h3>
-            <p className={css.description}>
+          <li className={styles.item}>
+            <div className={`${styles.icon} ${styles.iconUniversal}`}></div>
+            <h3 className={styles.cardTitle}>Універсальний дизайн</h3>
+            <p className={styles.description}>
               базові кольори та лаконічний стиль, що легко комбінуються між
               собою.
             </p>
           </li>
-          <li className={css.item}>
-            <div className={`${css.icon} ${css.iconComfort}`}></div>
-            <h3 className={css.cardTitle}>Комфорт на кожен день</h3>
-            <p className={css.description}>
+          <li className={styles.item}>
+            <div className={`${styles.icon} ${styles.iconComfort}`}></div>
+            <h3 className={styles.cardTitle}>Комфорт на кожен день</h3>
+            <p className={styles.description}>
               одяг, який не обмежує рухів і підходить для будь-якої ситуації.
             </p>
           </li>
