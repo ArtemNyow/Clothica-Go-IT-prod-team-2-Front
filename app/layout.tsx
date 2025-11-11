3074ccdcdd1a1d8c4af2b1b58d08e0242edf6db0
Yana Khovbosha <aemec4373@gmail.com>
2025-11-11 17:18:12 +0200

    add header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,19 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 import 'modern-normalize';
-import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
-import ToastProvider from "@/components/ToastProvider";
+import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
+import ToastProvider from '@/components/ToastProvider';
 
 const geistSans = Inter({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-  weight: ["400", "500", "600"], 
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+  weight: ['400', '500', '600'],
 });
 
-
 export const metadata: Metadata = {
-  title: "Clothica",
-  description: "Clothing store",
+  title: 'Clothica',
+  description: 'Clothing store',
   icons: {
     icon: '/favicon.ico',
   },
@@ -25,15 +24,15 @@ export default function RootLayout({
   modal,
 }: Readonly<{
   children: React.ReactNode;
-    modal: React.ReactNode;
+  modal: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable}`}>
         <TanStackProvider>
-        {children}
-        <ToastProvider />
-        {modal}
+          {children}
+          <ToastProvider />
+          {modal}
         </TanStackProvider>
       </body>
     </html>
